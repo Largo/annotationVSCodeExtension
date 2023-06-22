5743a4679ad4d089dae30300fbc23a01f9320ac1
Andi <largo@users.noreply.github.com>
2023-06-22 22:07:22 +0200

    working version of the extension. The first time a ruby file is called the annotation block will be collapsed

--- src/extension.ts
+++ src/extension.ts
@@ -3,30 +3,55 @@
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Your extension is activated the first time a ruby document is opened or if the command is called.
 export function activate(context: vscode.ExtensionContext) {
+	vscode.languages.registerFoldingRangeProvider('ruby', {
+		provideFoldingRanges(document: vscode.TextDocument, _context: vscode.FoldingContext, _token: vscode.CancellationToken) {
+			const foldingRanges: vscode.FoldingRange[] = [];
+			const startRegex = /^# == Schema Info/;
+			const endRegex = /^class\s/;
+	
+			let foldingStartLine: number | undefined = undefined;
+	
+			for (let i = 0; i < document.lineCount; i++) {
+				const line = document.lineAt(i);
+	
+				if (startRegex.test(line.text)) {
+					//console.log(i + " start of folding range");
+					// Start of a new folding range
+					foldingStartLine = i;
+				} else if (endRegex.test(line.text)) {
+					// End of the current folding range
+					if (foldingStartLine !== undefined) {
+						//console.log(i + " end of folding range");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "annotation" is now active!');
-
-	// // The command has been defined in the package.json file
-	// // Now provide the implementation of the command with registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('annotation.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from annotation!');
-	// });
-
-	// context.subscriptions.push(disposable);
-
-	const disposable = vscode.commands.registerCommand('extension.collapseABC', () => {
-		vscode.commands.executeCommand('editor.fold', { levels: 1 });
+						const foldingRange = new vscode.FoldingRange(foldingStartLine, i - 1, vscode.FoldingRangeKind.Region);
+						foldingRanges.push(foldingRange);
+						foldingStartLine = undefined;
+					}
+				}
+			}
+	
+			return foldingRanges;
+		}
 	});
 
+	const disposable = vscode.commands.registerCommand('annotation.collapseBlock', () => {
+		vscode.commands.executeCommand('editor.fold', { levels: 1 });
+	});
 	context.subscriptions.push(disposable);
 
+	 // Get the value of the extension.autoRunCommand setting
+	 const autoRunCommand = vscode.workspace.getConfiguration().get<boolean>('extension.autoRunCommand');
+
+	 if (autoRunCommand) {
+		 // Automatically run the command once when a Ruby file is opened
+		 vscode.workspace.onDidOpenTextDocument((document) => {
+			 if (document.languageId === 'ruby' || document.fileName.endsWith(".rb.git")) {
+				 vscode.commands.executeCommand('annotation.collapseBlock');
+			 }
+		 });
+	}
 }
 
 // This method is called when your extension is deactivated
